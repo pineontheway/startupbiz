b9a5116e6801f8ada170fe11ff54fbccd9f0a4b0
pineontheway <vijaymuniswamy9@gmail.com>
2016-12-04 22:53:27 -0800

    added angular code for hightcharts

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,25 @@ router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 
+router.get('/getData', function(req, res, next) {
+    var top_countries=[];
+    req.getConnection(function(err,connection){
+    var query = connection.query('SELECT Countries, Rounds FROM countries_by_rounds WHERE Year="2015"',function(err,rows)
+       {
+           if(err)
+               console.log("Error Selecting : %s ",err );
+           for (var i = 0; i < rows.length; i++) {
+                top_countries.push({
+                    name: rows[i].Countries,
+                    y: rows[i].Rounds
+                });
+            }
+            console.log(top_countries);
+            res.send(top_countries);
+        });
+        });
 
-
-
+});
 
 router.get('/dashboard',function(req,res){
     res.render("dashboard");
