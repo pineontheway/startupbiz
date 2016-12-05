fe39114ca06f5e5018069da65274641cc3a197b6
RihaGupta <rihagupta5992@gmail.com>
2016-12-04 23:13:57 -0800

    reverting back
    reverting back to static graph

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     req.getConnection(function(err,connection){
+
        var query = connection.query('SELECT * FROM countries_by_rounds',function(err,rows)
        {
            if(err)
@@ -13,27 +14,17 @@ router.get('/', function(req, res, next) {
     });
   res.render('index', { title: 'Express' });
 });
+
 router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 
-var top_countries = [];
+
+
+
+
 router.get('/dashboard',function(req,res){
-  req.getConnection(function(err,connection){
-       var query = connection.query('SELECT Countries, Rounds FROM countries_by_rounds WHERE Year="2015"',function(err,rows)
-       {
-           if(err)
-               console.log("Error Selecting : %s ",err ); 
-           for (var i = 0; i < rows.length; i++) {
-                top_countries.push({
-                    name: rows[i].Countries,
-                    y: rows[i].Rounds
-                }); 
-            };
-            console.log(top_countries);
-            res.render("dashboard", { top_countries: top_countries });
-        });
-    });
+    res.render("dashboard");
 });
 
 router.get('/acquisition',function(req,res){
@@ -49,10 +40,4 @@ router.get('/people',function(req,res){
     res.render("people");
 });
 
-router.get('/funding',function(req,res){
-    res.render("funding");
-});
-router.get('/startup',function(req,res){
-    res.render("startup");
-});
 module.exports = router;
